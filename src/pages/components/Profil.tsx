276630b6a91702d77030b7393663e6332c2c83ac
Rikuto <orikutobasuke@gmail.com>
2023-04-23 23:27:36 +0900

    Blog-Triral

--- src/pages/components/Profil.tsx
+++ src/pages/components/Profil.tsx
@@ -1,5 +1,16 @@
+import { Box, Flex, Text } from '@chakra-ui/react'
+
 export default function Profil() {
     return (
-        <></>
+        <>
+            <Box>
+                <Flex>
+                    <Box></Box>
+                    <Text>
+                        ECCコンピュータ専門学校に通う、2年生。<br />
+                    </Text>
+                </Flex>
+            </Box>
+        </>
     )
 }
