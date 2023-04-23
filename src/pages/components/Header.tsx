44fb55c1b4b4dfbe426e0e0ee36df7cb7463dfe2
Rikuto <orikutobasuke@gmail.com>
2023-04-23 21:17:41 +0900

    Blog-Trial

--- src/pages/components/Header.tsx
+++ src/pages/components/Header.tsx
@@ -1,7 +1,11 @@
 import { Box, Flex, Text, UnorderedList, ListItem } from '@chakra-ui/react'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 export default function Header() {
+
+    const router = useRouter()
+
     return (
         <>
             <header>
@@ -12,15 +16,18 @@ export default function Header() {
                         listStyleType={'none'}
                         justifyContent={'space-around'}
                     >
-                        <ListItem>
-                            <Link href={'/'}>Profile</Link>
-                        </ListItem>
-                        <ListItem>
-                            <Link href={'/'}>article</Link>
-                        </ListItem>
-                        <ListItem>
-                            <Link href={'/'}>Mail</Link>
-                        </ListItem>
+                        <ListItem
+                            onClick={() => router.push('/')}
+                            cursor={'pointer'}
+                        >Profile</ListItem>
+                        <ListItem
+                            onClick={() => router.push('/')}
+                            cursor={'pointer'}
+                        >article</ListItem>
+                        <ListItem
+                            onClick={() => router.push('/')}
+                            cursor={'pointer'}
+                        >Mail</ListItem>
                     </UnorderedList>
                 </Flex>
             </header>
