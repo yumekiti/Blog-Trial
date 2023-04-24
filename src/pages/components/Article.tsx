b69aad48450646c0a6071b822485ae50b4e50d58
Rikuto <orikutobasuke@gmail.com>
2023-04-24 16:59:39 +0900

    Blog-Trial

--- src/pages/components/Article.tsx
+++ src/pages/components/Article.tsx
@@ -1,11 +1,11 @@
-import { Box, Flex, Text } from '@chakra-ui/react'
+import { Box, Flex, Text, Button } from '@chakra-ui/react'
 
 export default function Articl() {
     return (
         <>
-            <Flex w={'100%'} h={'150rem'} flexDir={'column'} justifyContent={'center'} alignItems={'center'} marginTop={'50rem'}>
+            <Flex w={'100%'} h={'150rem'} flexDir={'column'} justifyContent={'center'} alignItems={'center'} marginTop={'50rem'} bg={'#fff'}>
                 <Text fontSize={'5xl'} marginBottom={'2rem'}>＜Articl＞</Text>
-                <Flex flexWrap={'wrap'} w={'100rem'} h={'145rem'} bg={'#fff'} boxShadow={'dark-lg'} gap={'4rem'} justifyContent={'center'} alignItems={'center'}>
+                <Flex flexWrap={'wrap'} w={'100rem'} h={'145rem'} boxShadow={'dark-lg'} gap={'4rem'} justifyContent={'center'} alignItems={'center'}>
                     <Box>
                         <Flex bg={'blue'} w={'40rem'} h={'30rem'} justifyContent={'center'} alignItems={'center'}>
                             <Text color={'#fff'} fontSize={'9xl'}>img</Text>
@@ -43,6 +43,15 @@ export default function Articl() {
                         <Text fontSize={'3xl'} marginTop={'2rem'}>React.jsについて</Text>
                     </Box>
                 </Flex>
+                <Button
+                    w={'20rem'}
+                    h={'4rem'}
+                    color={'#fff'}
+                    fontWeight={'bold'}
+                    marginTop={'3rem'}
+                    bgGradient={'linear(to-l,blue.200,purple.200)'}
+                    _hover={{ bgGradient: 'linear(purple.400,blue.400)' }}
+                >SHOW MORE</Button>
             </Flex>
         </>
     )
