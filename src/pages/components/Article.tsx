058d15c89b877aa6d8cf4954f696e9270862f93f
Rikuto <orikutobasuke@gmail.com>
2023-04-24 12:21:00 +0900

    Blog-Trial

--- src/pages/components/Article.tsx
+++ src/pages/components/Article.tsx
@@ -3,27 +3,43 @@ import { Box, Flex, Text } from '@chakra-ui/react'
 export default function Articl() {
     return (
         <>
-            <Flex w={'100%'} h={'150rem'} flexDir={'column'} justifyContent={'center'} alignItems={'center'}>
-                <Text></Text>
+            <Flex w={'100%'} h={'150rem'} flexDir={'column'} justifyContent={'center'} alignItems={'center'} marginTop={'40rem'}>
+                <Text fontSize={'5xl'} marginBottom={'2rem'}>＜Articl＞</Text>
                 <Flex flexWrap={'wrap'} w={'100rem'} h={'145rem'} bg={'#fff'} boxShadow={'dark-lg'} gap={'4rem'} justifyContent={'center'} alignItems={'center'}>
                     <Box>
-                        <Box bg={'blue'} w={'40rem'} h={'30rem'}></Box>
+                        <Flex bg={'blue'} w={'40rem'} h={'30rem'} justifyContent={'center'} alignItems={'center'}>
+                            <Text color={'#fff'} fontSize={'9xl'}>img</Text>
+                        </Flex>
                         <Text>HTMLについて</Text>
                     </Box>
                     <Box>
-                        <Box bg={'blue'} w={'40rem'} h={'30rem'}></Box>
+                        <Flex bg={'blue'} w={'40rem'} h={'30rem'} justifyContent={'center'} alignItems={'center'}>
+                            <Text color={'#fff'} fontSize={'9xl'}>img</Text>
+                        </Flex>
                         <Text>HTMLについて</Text>
                     </Box>
                     <Box>
-                        <Box bg={'blue'} w={'40rem'} h={'30rem'}></Box>
+                        <Flex bg={'blue'} w={'40rem'} h={'30rem'} justifyContent={'center'} alignItems={'center'}>
+                            <Text color={'#fff'} fontSize={'9xl'}>img</Text>
+                        </Flex>
                         <Text>HTMLについて</Text>
                     </Box>
                     <Box>
-                        <Box bg={'blue'} w={'40rem'} h={'30rem'}></Box>
+                        <Flex bg={'blue'} w={'40rem'} h={'30rem'} justifyContent={'center'} alignItems={'center'}>
+                            <Text color={'#fff'} fontSize={'9xl'}>img</Text>
+                        </Flex>
                         <Text>HTMLについて</Text>
                     </Box>
                     <Box>
-                        <Box bg={'blue'} w={'40rem'} h={'30rem'}></Box>
+                        <Flex bg={'blue'} w={'40rem'} h={'30rem'} justifyContent={'center'} alignItems={'center'}>
+                            <Text color={'#fff'} fontSize={'9xl'}>img</Text>
+                        </Flex>
+                        <Text>HTMLについて</Text>
+                    </Box>
+                    <Box>
+                        <Flex bg={'blue'} w={'40rem'} h={'30rem'} justifyContent={'center'} alignItems={'center'}>
+                            <Text color={'#fff'} fontSize={'9xl'}>img</Text>
+                        </Flex>
                         <Text>HTMLについて</Text>
                     </Box>
                 </Flex>
