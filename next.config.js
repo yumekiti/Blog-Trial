0609eaf674b86f24b6bb682c1377514f214554dd
yumekiti <yumekiti120440@gmail.com>
2023-05-09 06:35:58 +0900

    `test` 環境変数の検証

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: process.env.GITHUB_ACTIONS && "/Blog-Trial",
-  trailingSlash: true,
 }
 
 module.exports = nextConfig
