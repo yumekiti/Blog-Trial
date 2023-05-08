52f32fa954ad58c87c193b2dc68cf4312d4ce202
yumekiti <yumekiti120440@gmail.com>
2023-05-09 06:19:22 +0900

    `add` BRANCH_NAMEの環境変数に対応させた

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
-const urlPrefix = process.env.BRANCH_NAME ? '/' + process.env.BRANCH_NAME : ''
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: urlPrefix,
-  assetPrefix: urlPrefix,
 }
 
 module.exports = nextConfig
