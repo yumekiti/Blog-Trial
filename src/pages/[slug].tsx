7e3191582870da630c1bfd834501bb1a0ea3814d
Rikuto <orikutobasuke@gmail.com>
2023-05-10 23:59:43 +0900

    Blog-Trial

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -44,7 +44,7 @@ export default function Post({ postData }: PostProps) {
   const { title, date } = postData.data
   return (
     <div>
-      <h1>{title}</h1>
+      <h1 className='title'>{title}</h1>
       <div>{date}</div>
       <div dangerouslySetInnerHTML={{ __html: postData.content }} />
     </div>
